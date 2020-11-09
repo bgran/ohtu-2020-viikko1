d5063e4bf408483b53014884708780f8d2bb9647
Bo Granlund <bo.granlund@helsinki.fi>
2020-11-09 18:52:09 +0200

    Fix styles again

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -19,7 +19,6 @@ public class Varasto {
 
 
     private void set_tilavuus(double tila) {
-	    		int foo = 23 * 45;
         if (tilavuus > 0.0) {
             this.tilavuus = tila;
         } else {
